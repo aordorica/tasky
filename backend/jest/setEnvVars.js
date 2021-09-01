c978987b9ea123096c90ad4fdc41dedc0106ebf6
Alan Ordorica <41565814+aordorica@users.noreply.github.com>
2021-09-01 16:20:50 -0700

    Delete backend/jest directory

--- backend/jest/setEnvVars.js
+++
@@ -1,5 +0,0 @@
-process.env.MONGODB_CONNECT_KEY =
-    "mongodb://127.0.0.1:27017/tasky_app_api_test";
-process.env.JWT_SECRET = 'taskysecretkey';
-process.env.SENDGRID_API_KEY='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'
-process.env.PORT = 3000;
